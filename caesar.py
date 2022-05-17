80082e30ef087d1c988a8b3a7a998c54dc99334e
Jstar414370153 <45889798+Jstar414370153@users.noreply.github.com>
2022-05-16 20:31:17 -0500

    2nd

--- caesar.py
+++ caesar.py
@@ -1,12 +1,23 @@
 
 def encrypt(key,plaintext):
     ciphertext=""
-    ciphertext = plaintext
+    for letter in plaintext:
+        if letter in 'abcdefghijklmnopqrstuvwxyz':
+            temp = ord(letter) - ord('a') + key 
+            temp %= 26
+            ciphertext += chr(temp + ord('a'))
+        elif letter in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            temp = ord(letter) - ord('A') + key
+            temp %= 26
+            ciphertext += chr(temp + chr('A'))
+        else:
+            ciphertext += letter
     return ciphertext
 
 def decrypt(key,ciphertext):
     plaintext=""
-    plaintext = ciphertext
+    for letter in ciphertext:
+        plaintext += letter
     return plaintext
 
 
