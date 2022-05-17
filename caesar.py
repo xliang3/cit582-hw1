1edee1190b608325263f004a3f5ecc0f57b4f515
Jstar414370153 <45889798+Jstar414370153@users.noreply.github.com>
2022-05-16 20:38:37 -0500

    6th

--- caesar.py
+++ caesar.py
@@ -3,31 +3,35 @@ def encrypt(key,plaintext):
     ciphertext=""
     for letter in plaintext:
         if letter in 'abcdefghijklmnopqrstuvwxyz':
-            temp = ord(letter) - ord('a') 
+            temp = ord(letter) - ord('a') + key
             temp %= 26
             ciphertext += chr(temp + ord('a'))
         elif letter in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
-            temp = ord(letter) - ord('A')
+            temp = ord(letter) - ord('A') + key
             temp %= 26
-            ciphertext += chr(temp + chr('A'))
+            ciphertext += chr(temp + ord('A'))
         else:
             ciphertext += letter
     print(plaintext)
+    print(ciphertext)
     return ciphertext
 
 def decrypt(key,ciphertext):
     plaintext=""
     for letter in ciphertext:
         if letter in 'abcdefghijklmnopqrstuvwxyz':
-            temp = ord(letter) - ord('a') + 26
+            temp = ord(letter) - ord('a') + 26 - key
             temp %= 26
             plaintext += chr(temp + ord('a'))
         elif letter in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
-            temp = ord(letter) - ord('A') + 26
+            temp = ord(letter) - ord('A') + 26 - key
             temp %= 26
-            plaintext += chr(temp + chr('A'))
+            plaintext += chr(temp + ord('A'))
         else:
             plaintext += letter
+    print(ciphertext)
+    print(plaintext)
     return plaintext
 
 
+# decrypt(1, encrypt(1, "wahaSDFWEF22rfd!@#ha"))
